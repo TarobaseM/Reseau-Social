ba2a66d7d5ea6906c82b5c7f4744385b9d615ce8
Tom Amar <95186955+TarobaseM@users.noreply.github.com>
2023-10-21 00:57:11 +0200

    style amelioré ++

--- PostApp/models.py
+++ PostApp/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField(max_length=1000)
+    content = models.TextField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     image=models.ImageField(upload_to="images_postes/",blank=True,null=True)
     likes = models.ManyToManyField(User, related_name='liked_publications')
@@ -13,3 +13,10 @@ class Post(models.Model):
         return f"Post by {self.author.username} at {self.created_at}"
     def total_likes(self):
         return self.likes.count()
+    def get_image_width(self):
+        with Post.open(self.image.path) as img:
+            return img.width
+
+    def get_image_height(self):
+        with Post.open(self.image.path) as img:
+            return img.height
